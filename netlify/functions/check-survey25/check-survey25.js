c66919ef7d5915e546230b3f67e82de0ae2c4e67
MAB <madeline.blount@gmail.com>
2025-03-17 17:40:03 -0400

    check function, expired survey

---
+++ netlify/functions/check-survey25/check-survey25.js
@@ -0,0 +1,36 @@
+const Airtable = require('airtable');
+
+exports.handler = function(event, context, callback) {
+  const data = JSON.parse(event.body);
+  const token = data.token;
+
+  var base = new Airtable({ apiKey: process.env.AIRTABLE_DATADVOCACY_TOKEN }).base('appiQkR2Zrww4DQnz');
+  // DATA/ADVOCACY BASE
+
+  let found = false;
+
+  // query airtable,
+  // check for token in submission list
+
+  base('Demographics Survey').select({
+    fields: ["token"],
+    filterByFormula: `{token}="${token}"`
+  }).eachPage(function page(records, fetchNextPage) {
+    records.forEach(function (record) {
+      if (record.get("token") == token) {
+        found = true;
+      }
+
+      // If there are no more records, `done` will get called.
+      fetchNextPage();
+    }, async function done(err) {
+      if (err) { console.error(err); return; }
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+          found: found
+        })
+      });
+    });
+  });
+}
